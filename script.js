3c8bf969738e3e960fd7a79a82222a2c1dd0fa1d
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-26 13:52:53 -0400

    added event listener which dynamically creates and adds the divs to according the user input, removed the div styles in style.css and added them in script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+let gridSize = prompt("What is the grid size?");
+let size = 480/gridSize;
+
+const containerForGrids = document.querySelector('#container');
+
+const generateGrids = document.querySelector('#gridGeneration');
+generateGrids.addEventListener('click', () => {
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const grids = document.createElement('div');
+            grids.setAttribute('style', `height: ${size}px; width: ${size}px; outline: 1px solid black;`);
+            grids.classList.add('grid');
+            containerForGrids.appendChild(grids);
+
+        }
+    }
+});
