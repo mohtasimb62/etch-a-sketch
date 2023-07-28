c439e88a7b0edfe7103e35661e86fbb9499f3f21
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-27 23:17:02 -0400

    implemented grid ONLY using flexbox

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let gridSize = prompt("What is the grid size?");
-let size = 720/gridSize;
 
-const containerForGrids = document.querySelector('#container');
+const containerForGrid = document.querySelector('#container');
 
 const generateGrids = document.querySelector('#gridGeneration');
 generateGrids.addEventListener('click', () => {
 
     for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const grids = document.createElement('div');
-            grids.setAttribute('style', `height: ${size}px; width: ${size}px; outline: 1px solid black;`);
-            grids.classList.add('grid');
-            containerForGrids.appendChild(grids);
-
-        }
+        const columns = document.createElement('div');
+        columns.classList.add('column');
+        columns.style.cssText = "display: flex; flex: 1";
+        containerForGrid.appendChild(columns);
     }
-});
 
-const fillColour = document.querySelector('#container');
-fillColour.addEventListener('mousemove', () => {
-    fillColour.setAttribute('style', 'backgroud-color: red');
+    const addingRows = document.getElementsByClassName('column');
+    for (let k = 0; k < gridSize; k++) {
+        for (let j = 0; j < addingRows.length; j++) {
+            const rows = document.createElement('div');
+            rows.classList.add('row');
+            rows.style.cssText = "flex: 1; outline: 1px solid black";
+            addingRows[j].appendChild(rows);
+        }    
+    }
+    
+    
+    // const allGrids = document.querySelectorAll('.grid');
+    // allGrids.forEach(grid => {
+    //     grid.setAttribute('style', 'outline: 1px solid black; flex-wrap: wrap; flex: 1 0 auto;');
+    //     grid.addEventListener('mouseenter', () => {
+    //         grid.style.backgroundColor = 'red';
+    //     });
+    // });
 });
